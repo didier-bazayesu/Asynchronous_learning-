7775bc6af102d71174d32705c245ad0cc5fe6b49
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-10 09:49:28 +0200

    feat: the 3rd question

--- fetchingUserDetails.js
+++ fetchingUserDetails.js
@@ -1,22 +1,39 @@
 async function fecthUserDetails(){
-    let userDetails = await fetch('https://jsonplaceholder.typicode.com/users');
-    let data = await userDetails.json();
-   
-  const information =  data.map(async ele=> {
-        
-        let post = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${ele.id}`);
-        let data = await post.json();
-        console.log(`username : ${ele.username}
-        Email : ${ele.email}`);
-        console.log('post:');
-        data.forEach(ele=> {
-            console.log(ele.title);
-        })
-        console.log('\n');
-        
-    })
 
-   await Promise.all(information);
+    try{
+        let userDetails = await fetch('https://jsonplaceholder.typicode.com/users');
+        let data = await userDetails.json();
+       
+      const information =  data.map(async ele=> {
+            
+            let post = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${ele.id}`);
+            let data = await post.json();
+            let id = null;
+            data.map(ele=> {
+                id = ele.id;
+            })
+
+
+            console.log(`username : ${ele.username}
+            Email : ${ele.email}`);
+            console.log('post:');
+            data.forEach(ele=> {
+                if(ele.id === id){
+
+                    console.log(ele.title);
+                }
+                
+            })
+            console.log('\n');
+            
+        })
+    
+       await Promise.all(information);
+
+
+    }catch(error){
+        console.error(error);
+    }
 
 
 };
