ad6239f959bac47a430026357e1d0a1cee0628e6
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-10 11:09:36 +0200

    feat: handle minnor error

--- countryDetails.js
+++ countryDetails.js
@@ -1,17 +1,23 @@
 async function fetchCountryDetails(countryName){
-    let countryDetails = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-    let country = await countryDetails.json();
-    country.forEach(async ele=>{
-        console.log(ele.name.common);
-        const [lat,lon] = ele.latlng
-        let weather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`)
-        let data =  await weather.json();
-         console.log(ele.capital[0]);
-        console.log(`${data?.current_weather?.temperature} °C`)
-       
-    })
+    try{
+        let countryDetails = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+        let country = await countryDetails.json();
+        country.forEach(async ele=>{
+            console.log(ele.name.common);
+            const [lat,lon] = ele.latlng
+            let weather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`)
+            
+            let data =  await weather.json();
+             console.log(ele.capital[0]);
+            console.log(`${data?.current_weather?.temperature} °C`)
+           
+        })
+    
+    await Promise.all(country)
 
-await Promise.all(country)
+    }catch(err){
+        console.error(err);
+    }
 // .then(console.log)
 }
 
