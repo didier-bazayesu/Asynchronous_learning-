60f9f9a76f4c4ccc2d393079b6388ff259f773d3
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-10 09:32:37 +0200

    feat: fetching data post

--- fetchingUserDetails.js
+++ fetchingUserDetails.js
@@ -1,18 +1,24 @@
 async function fecthUserDetails(){
     let userDetails = await fetch('https://jsonplaceholder.typicode.com/users');
     let data = await userDetails.json();
-   return  data.map(async ele=> {
+   
+  const information =  data.map(async ele=> {
         
         let post = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${ele.id}`);
         let data = await post.json();
         console.log(`username : ${ele.username}
         Email : ${ele.email}`);
-      
-        
-
+        console.log('post:');
+        data.forEach(ele=> {
+            console.log(ele.title);
+        })
+        console.log('\n');
         
     })
- 
+
+   await Promise.all(information);
+
+
 };
 
 
