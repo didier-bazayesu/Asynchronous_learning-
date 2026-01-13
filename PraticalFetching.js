d9528190c5b35e9c8c78fa6da065d4ecad374ec5
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-13 09:40:54 +0200

    feat: fetching the specific user with specific post

---
+++ PraticalFetching.js
@@ -0,0 +1,40 @@
+
+async function PraticalFetching() {
+
+    try{
+
+
+        let data = await fetch('https://jsonplaceholder.typicode.com/users');
+        if(!data.ok){
+            throw new Error("there something behind");
+        }
+        let response = await data.json();
+    
+        response.forEach(async element => {
+           
+            let post = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${element.id}`);
+            let result =  await post.json();
+             console.log("User",element.name);
+             console.log("Email:",element.email);
+    
+             //returning post
+             console.log("Post:")
+             result.forEach(element=> {
+                console.log(element.title);
+             })
+            
+             console.log('\n');
+        });
+    
+    
+       await Promise.all(response);
+
+    }catch(error){
+        console.error(error);
+    }
+
+
+}
+
+PraticalFetching().then(console.log)
+
