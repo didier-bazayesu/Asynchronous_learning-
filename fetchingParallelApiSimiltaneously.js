1928f7195c0fa4ff48c188eaa64d505593abe38a
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-13 10:23:24 +0200

    feat:handling someError here

--- fetchingParallelApiSimiltaneously.js
+++ fetchingParallelApiSimiltaneously.js
@@ -1,30 +1,32 @@
 
-
-
-
 async function fetchingParallelApiSimiltaneously() {
+   try{
+       const user = await fetch('https://jsonplaceholder.typicode.com/users');
+       const userDetails = await user.json();
+   
+   
+       //fetching the comment 
+   
+       const post = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const postDetails = await post.json();
+   
+   
+        //fetching comments 
+   
+       const comment = await fetch("https://jsonplaceholder.typicode.com/comments");
+       const commentDetais = await comment.json();
+   
+       return await Promise.all([userDetails,postDetails,commentDetais]);
 
-    const user = await fetch('https://jsonplaceholder.typicode.com/users');
-    const userDetails = await user.json();
-
-
-    //fetching the comment 
-
-    const post = await fetch("https://jsonplaceholder.typicode.com/posts")
-     const postDetails = await post.json();
-
-
-     //fetching comments 
-
-    const comment = await fetch("https://jsonplaceholder.typicode.com/comments");
-    const commentDetais = await comment.json();
-
-    return await Promise.all([userDetails,postDetails,commentDetais]);
+   }catch(error){
+    console.error(error);
+   }
 }
 
 
 fetchingParallelApiSimiltaneously().then(([user,comment,result])=>{
-    console.log("userLength",user.length);
-    console.log("commentLength",comment.length);
-    console.log("post result",result.length);
+    console.log(`commentuser:${user.length>=1 ? user.length : "No data fetched"}`);
+    console.log(`commentLength:${comment.length>=1 ? comment.length :"No data fetched" }`);
+    console.log(`commentLength:${result.length>=1 ? result.length : "No data fetched"}`);
+    
 });
