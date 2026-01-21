594c7665a397be20ee4ecc01543ddab43f2efc49
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-21 10:41:19 +0200

    feat: fetching in one line

--- learnAsync.js
+++ learnAsync.js
@@ -1,31 +1,3 @@
-import { handleFetch } from "./fetch3Api.js";
-
-// async function handlefetching (){
-//     try{
-//         let data = await fetch('https://jsonplaceholder.typicode.com/users');
-        
-//         if(data.ok){
-//             return await data.json();
-
-//         }
-//     }catch(error){
-//         console.error(error);
-//     }
-
-// }
-//  async function handlefetching() {
-//     try {
-//         let data = fetch('https://jsonplaceholder.typicode.com/users').then(data=> data.json());
-//         return data;
-
-      
-//     } catch (error) {
-//         console.error(error);
-//     }
-
-// }
-
-
-// handlefetching().then(console.log);
-
- handleFetch("https://jsonplaceholder.typicode.com/users").then(console.log).catch(console.log);
+(async function handleFetch(){
+    console.log(await (await fetch('https://jsonplaceholder.typicode.com/users')).json())       
+})()
