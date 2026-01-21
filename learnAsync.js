1d2923f877b8036d3a808291ef202424ae042af7
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-21 10:46:33 +0200

    feat: fetching data without invoked function

--- learnAsync.js
+++ learnAsync.js
@@ -1,3 +1,7 @@
-(async function handleFetch(){
-    console.log(await (await fetch('https://jsonplaceholder.typicode.com/users')).json())       
-})()
+async function handleFetch(){
+    const data = await fetch('https://jsonplaceholder.typicode.com/users')
+    const result = await data.json()
+    console.log(result)
+} 
+
+ handleFetch()
