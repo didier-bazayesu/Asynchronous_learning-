28c83eab2004872f995d6b65fbed05c72b8f2bcf
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-23 11:37:01 +0200

    feat: learning Async

--- learnAsync.js
+++ learnAsync.js
@@ -1,7 +1,8 @@
-async function handleFetch(){
-    const data = await fetch('https://jsonplaceholder.typicode.com/users')
-    const result = await data.json()
-    console.log(result)
-} 
+setImmediate(() => {
+    console.log('setImmediate');
+});
 
- handleFetch()
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0);
+
