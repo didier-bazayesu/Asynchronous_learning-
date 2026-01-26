a90d290392ca2914222eb502d548a97f4852d7ab
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-26 20:07:06 +0200

    feat: comvert promise into async

--- fetch.js
+++ fetch.js
@@ -1,7 +1,36 @@
-(async function handlefetch(url){
- let data = await((await fetch(url))).json()
- console.log(data
+// (async function handlefetch(url){
+//  let data = await((await fetch(url))).json()
+//  console.log(data
 
- )
-})("https://jsonplaceholder.typicode.com/users")
+//  )
+// })("https://jsonplaceholder.typicode.com/users")
 
+
+// Convert a Promise chain into an async/await function.
+
+function fetchCallback (url){
+   return  fetch(url).then(response=> {
+    if(response.ok) return response
+    else throw new Error("fetch Error")
+   }).then(data=> {
+    return data.json()
+   })
+}
+fetchCallback("https://jsonplaceholder.typicode.com/users").then(e=>{
+    const dataId = e.map(elem=>elem.id);
+    return dataId;
+}).then(console.log)
+
+
+//using asynch code 
+
+
+async function fetchCallback(url){
+    try{
+        console.log((await(await fetch(url)).json()).map(elem=> elem.id)); 
+
+    }catch(error){
+        console.error(error)
+    }
+}
+fetchCallback("https://jsonplaceholder.typicode.com/users");
