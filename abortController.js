7369547bd39efebf449b0922c4b7eca90cf7f620
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-02 12:48:38 +0200

    feat: abort controller

--- abortController.js
+++ abortController.js
@@ -1,25 +1,25 @@
 async function handleAbort() {
-    
     const controller = new AbortController();
     const signal = controller.signal;
     
-    setTimeout(()=>{
-       controller.abort();
-    },2000)
-
-    try{ 
-        const response = await fetch("https://jsonplaceholder.typicode.com/users",{signal});
-
+    try {
+        
+        
+        const response = await fetch("https://jsonplaceholder.typicode.com/users", { signal });
+        
         const data = await response.json();
-
-
+        
+        
         return data;
-
-    }catch(error){
-        if(error.name === "AbortError") console.log("fetch aborted");
+        
+    } catch (error) {
+        if (error.name === "AbortError") console.log("fetch aborted");
         else return console.error(error)
     }
     
+    setTimeout(() => {
+        controller.abort();
+    }, 100)
 }
 
 handleAbort().then(console.log)
