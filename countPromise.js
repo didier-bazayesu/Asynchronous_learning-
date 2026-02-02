7f7bebe2f9486eb3ef270b67572b75ca64f68106
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-02 12:49:53 +0200

    feat: countPromise

--- countPromise.js
+++ countPromise.js
@@ -1,35 +1,50 @@
 async function asyncCounter(){
     
 return new Promise((resolve,reject)=>{
-    try{
-            let start = 0 ;
-            let array =  [];
-            let controller = new AbortController().signal;
+    // try{
+    //         let start = 0 ;
+    //         let array =  [];
+    //         let controller = new AbortController().signal;
             
         
-            let interval = setInterval(()=>{
-                console.log(start);
-               start++;
-               array.push(start);
-            },1000)
-            setTimeout(()=>{
-            clearInterval(interval);
-            resolve(array)
-            },6000)
+    //         let interval = setInterval(()=>{
+    //             console.log(start);
+    //            start++;
+    //            array.push(start);
+    //         },1000)
+    //         setTimeout(()=>{
+    //         clearInterval(interval);
+    //         resolve(array)
+    //         },6000)
 
 
 
         
-    }catch(error){
-        console.error(error);
-        reject(error);
-    };
+    // }catch(error){
+    //     console.error(error);
+    //     reject(error);
+    // };
 
 
-     })
+    //  })
 
-   
+let count =0 ;
+let newArray = [];
+
+let intervalId = setInterval(()=> {
+    count++;
+    newArray.push(count);
+    console.log(count);
+    if(count == 5){
+        resolve(newArray)
+        clearInterval(intervalId) }
+
+        
+
+},1000);
+
+})  
     
 }
 
-asyncCounter().then(dta=> console.log(dta));
+asyncCounter().then(dta=> console.log(dta)).catch(console.log);
