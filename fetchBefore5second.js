26225e7c7f87a1f53c1a9aa04ca846e49a79c0b9
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-04 23:20:28 +0200

    feat: aborting fetch

---
+++ fetchBefore5second.js
@@ -0,0 +1,17 @@
+async function fetching_before_5_second(){
+    const controller = new AbortController();
+    const signal = controller.signal;
+    setTimeout(()=>{
+         controller.abort()
+    },1000)
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/users",{signal});
+        return response.json();
+
+    }catch(error){
+      console.error("fetch aborted",error)
+    };
+    
+
+}
+fetching_before_5_second().then(console.log)
